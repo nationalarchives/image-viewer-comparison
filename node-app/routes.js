3180ee4061dac021bf2611d46de5badd5aa4fced
Punal Chotrani <punalchotrani@Punals-MacBook-Pro.local>
2019-02-01 16:22:32 +0000

    Updated node app

---
+++ node-app/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const router = express.Router();
+
+// route to our app
+router.get('/', (req, res) => {
+    //res.send('Using express and nodemon');
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
+router.get('/image', (req, res) => {
+    res.send('image page');
+});
+
+module.exports = router;
--- node-app/server.js
+++ node-app/server.js
@@ -4,12 +4,13 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+
+// route our app
+const router = require('./routes');
+app.use('/', router);
+
+
 // start the server
 app.listen(port, () => {
     console.log('Have no fear EXPRESS is here.');
-});
-
-// route to our app
-app.get('/', (req, res) => {
-    res.send('Using express and nodemon');
 });
