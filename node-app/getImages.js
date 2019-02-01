ff827103eb7bfa852ed046fb56ea917101797187
Punal Chotrani <punalchotrani@Punals-MacBook-Pro.local>
2019-02-01 17:40:29 +0000

    Getting the image objects

--- node-app/getImages.js
+++ node-app/getImages.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 axios
     .get(`http://127.0.0.1:3000/manifest.json`)
     .then(response => {
-        console.log(response);
+        console.log(response.data.sequences[0]);
     })
     .catch((error) => {
         console.log(error);
