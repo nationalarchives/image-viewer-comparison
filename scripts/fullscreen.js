b50e4611e628f86d7c4e7b41a0c0780e4085547c
JamesWChan <jwchan1993@gmail.com>
2019-04-23 15:20:32 +0100

    Added full screen image to full screen button

--- scripts/fullscreen.js
+++ scripts/fullscreen.js
@@ -1,28 +1,43 @@
-setTimeout(() => {
-    let tifyScreen = document.querySelector("tify");
+function addFullscreenButton() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            let tifyScreen = document.getElementById("tify");
+            let createFullscreenButton = () => {
+                let buttons = document.querySelector(".tify-scan_buttons");
+                let fullscreenBtn = document.createElement("Button");
+                let fullscreenBtnImage = document.createElement('img');
+                let span = document.createElement('span');
 
-    let addFullscreenButton = () => {
-        let buttons = document.querySelector(".tify-scan_buttons");
-        let fullscreenBtn = document.createElement("Button");
+                buttons.appendChild(fullscreenBtn);
+                fullscreenBtn.setAttribute("title", "Full screen");
+                fullscreenBtn.setAttribute("class", "tify-scan_button");
 
-        buttons.appendChild(fullscreenBtn);
-        fullscreenBtn.setAttribute("title", "Full screen");
-        fullscreenBtn.setAttribute("class", "tify-scan_buttons");
+                fullscreenBtnImage.setAttribute('src',"../tify/img/baseline-fullscreen-24px.svg");
+                fullscreenBtn.appendChild(fullscreenBtnImage);
 
-        fullscreenBtn.addEventListener("click", (e) => {
-            if (tifyScreen.requestFullscreen) {
-                tifyScreen.requestFullscreen();
-            } else if (tifyScreen.mozRequestFullScreen) { // Firefox
-                tifyScreen.mozRequestFullScreen();
-            } else if (tifyScreen.webkitRequestFullscreen) { // Chrome, Safari and Opera
-                tifyScreen.webkitRequestFullscreen();
-            } else if (tifyScreen.msRequestFullscreen) { // IE/Edge
-                tifyScreen.msRequestFullscreen();
-            }
-        });
-    };
+                span.setAttribute('class', 'tify-sr-only');
+                fullscreenBtn.appendChild(span);
 
-    if(tifyScreen !== null){
-        addFullscreenButton();
-    }
-}, 100);
+                fullscreenBtn.addEventListener("click", (e) => {
+                    if (tifyScreen.requestFullscreen) {
+                        tifyScreen.requestFullscreen();
+                    } else if (tifyScreen.mozRequestFullScreen) { // Firefox
+                        tifyScreen.mozRequestFullScreen();
+                    } else if (tifyScreen.webkitRequestFullscreen) { // Chrome, Safari and Opera
+                        tifyScreen.webkitRequestFullscreen();
+                    } else if (tifyScreen.msRequestFullscreen) { // IE/Edge
+                        tifyScreen.msRequestFullscreen();
+                    }
+                });
+            };
+
+            createFullscreenButton();
+        }, 100);
+    });
+}
+
+async function asyncCall() {
+    let result = await addFullscreenButton();
+}
+
+asyncCall();
