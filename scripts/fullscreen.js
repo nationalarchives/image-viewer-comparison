5b05e42b9b37c74c04be7c774df429390bff620c
JamesWChan <jwchan1993@gmail.com>
2019-04-23 13:03:11 +0100

    Added script to provide full screen functionality for TIFY

---
+++ scripts/fullscreen.js
@@ -0,0 +1,26 @@
+setTimeout(() => {
+    let tifyScreen = document.querySelector('#tify');
+
+    let addFullscreenButton = () => {
+        let buttons = document.querySelector('.tify-scan_buttons');
+        let fullscreenButton = document.createElement('Button');
+
+        buttons.appendChild(fullscreenButton);
+
+        fullscreenButton.addEventListener('click', (e) => {
+            if (tifyScreen.requestFullscreen) {
+                tifyScreen.requestFullscreen();
+            } else if (tifyScreen.mozRequestFullScreen) { // Firefox
+                tifyScreen.mozRequestFullScreen();
+            } else if (tifyScreen.webkitRequestFullscreen) { // Chrome, Safari and Opera
+                tifyScreen.webkitRequestFullscreen();
+            } else if (tifyScreen.msRequestFullscreen) { // IE/Edge
+                tifyScreen.msRequestFullscreen();
+            }
+        });
+    };
+
+    if(tifyScreen !== null){
+        addFullscreenButton();
+    }
+}, 100);
